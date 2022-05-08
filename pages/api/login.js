13fcf49fd8287a5684b598c119c055f63b1b06fb
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-05-07 23:46:22 -0300

    clean up login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,32 +2,10 @@ import { setAuthCookies } from "next-firebase-auth";
 import initAuth from "../../utils/initAuth";
 import initB from "../../utils/initB";
 
-import { getDatabase, ref, set } from "firebase/database";
-import firebase from "firebase";
-
-initAuth();
 initB();
-
-// Get a reference to the database service
-var database = firebase.database();
+initAuth();
 
 const handler = async (req, res) => {
-  function markTicketAsUsed(contractAddress, tokenId) {
-    console.log("/////////////");
-    firebase
-      .database()
-      .ref(`tickets/${contractAddress}/used/${tokenId}`)
-      .set(true);
-    
-    var tickets = firebase.database().ref(`tickets/${contractAddress}`);
-    tickets.on('value', (ticket) => {
-      const data = ticket.val();
-      console.log(data)
-    });
-    console.log('/////////////')
-  }
-  markTicketAsUsed('0xa', 'b');
-  markTicketAsUsed('0xa', 'c');
 
   try {
     await setAuthCookies(req, res);
