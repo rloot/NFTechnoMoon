8dc2381c2ed7ee0325736cdd6d0c08ec1425a51a
scammi <scammi@gmail.com>
2022-05-08 01:44:06 -0300

    Ticket utils

--- pages/api/validate.js
+++ pages/api/validate.js
@@ -1,7 +1,8 @@
 import { setAuthCookies } from "next-firebase-auth";
 import initAuth from "../../utils/initAuth";
 import initB from "../../utils/initB";
-import admin from 'firebase-admin'
+import admin from 'firebase-admin';
+import { isTicketUsed, markTicketAsUsed } from '../../utils/ticket';
 import firebase from "firebase";
 import * as _ from 'lodash'
 
@@ -10,42 +11,13 @@ initAuth();
 
 const handler = async (req, res) => {
 
-  function markTicketAsUsed(contractAddress, tokenId) {
-    firebase
-      .database(admin.apps[0])
-      .ref(`tickets/${contractAddress}/used/${tokenId}`)
-      .set(true);
-    
-    var tickets = firebase.database().ref(`tickets/${contractAddress}`);
-    tickets.on('value', (ticket) => {
-      const data = ticket.val();
-      console.log(data)
-    });
-  }
-
-  function isUsed(contractAddress, tokenId) {
-    const database = firebase.database(admin.apps[0])
-
-    database.ref(`tickets/${contractAddress}/used/${tokenId}`)
-      .get()
-      .then((snapshot) => {
-        if (snapshot.exists()) {
-          console.log(snapshot.val());
-        } else {
-          console.log("No data available");
-        }
-      }).catch((error) => {
-        console.error(error);
-      });  
-  }
-
   console.log(req.params)
  
   markTicketAsUsed('0xa', 'b');
   markTicketAsUsed('0xa', 'c');
-  isUsed('0xa', 'a')
-  isUsed('0xa', 'b')
-  isUsed('0xa', 'c')
+  isTicketUsed('0xa', 'a')
+  isTicketUsed('0xa', 'b')
+  isTicketUsed('0xa', 'c')
 
 };
 
---
+++ utils/isTicketUsed.js
@@ -0,0 +1,38 @@
+import admin from 'firebase-admin'
+import firebase from "firebase";
+
+const markTicketAsUsed = (contractAddress, tokenId) => {
+  const database = firebase.database(admin.apps[0])
+
+  firebase
+    .database(admin.apps[0])
+    .ref(`tickets/${contractAddress}/used/${tokenId}`)
+    .set(true);
+  
+  var tickets = firebnase.database().ref(`tickets/${contractAddress}`);
+  tickets.on('value', (ticket) => {
+    const data = ticket.val();
+    console.log(data)
+    return data;
+  });
+}
+
+const isTicketUsed = (contractAddress, tokenId) => {
+  const database = firebase.database(admin.apps[0])
+
+  database.ref(`tickets/${contractAddress}/used/${tokenId}`)
+  .get()
+  .then((snapshot) => {
+    if (snapshot.exists()) {
+      const snap = snapshot.val()
+      console.log(snap)
+      return snap;
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });  
+}
+
+export { markTicketAsUsed, isTicketUsed }
