ae7ceb21afbaa4260096df5b8c8e543ae6f7d5be
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-05-07 23:44:30 -0300

    avoid reinitializing app

--- utils/initB.js
+++ utils/initB.js
@@ -1,21 +1,21 @@
 /* globals window */
-import firebase from 'firebase'
-let initialized = false;
+import firebase from "firebase";
+import admin from "firebase-admin";
 
 const initB = () => {
-  if (initialized) return;
-  firebase.initializeApp({
-    
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    
-    storageBucket: "aaaa.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
-  })  
-  initialized  = true;  
-}
+  // console.log(admin.apps.length, admin.apps[0]);
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 
-export default initB
+      storageBucket: "aaaa.com",
+      messagingSenderId: "SENDER_ID",
+      appId: "APP_ID",
+    });
+  }
+};
+
+export default initB;
